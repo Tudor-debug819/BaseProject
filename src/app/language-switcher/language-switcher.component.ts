5703b5263786499296e06fc1affb60c85ba65a73
Tudor-debug819 <tovidiua@emeal.nttdata.com>
2025-01-20 09:41:42 +0200

    Update language-switcher.component.ts

--- src/app/language-switcher/language-switcher.component.ts
+++ src/app/language-switcher/language-switcher.component.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Language } from './language-switcher.service';
 
 @Component({
   selector: 'app-language-switcher',
+  standalone: true,
   imports: [],
   templateUrl: './language-switcher.component.html',
   styleUrl: './language-switcher.component.css'
 })
 export class LanguageSwitcherComponent {
+  @Input() enabled: boolean = true; // Determines if the switcher is visible
+  @Input() languages: Language[] = []; // List of languages
+
+  changeLanguage(language: Language): void {
+    if (language.enabled) {
+      console.log(`Language changed to: ${language.name}`);
+      
+    }
+  }
 
 }
